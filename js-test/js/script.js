445a106c3551d837cfbc48adb773f3e74a9004f1
h.ogino.258 <ogna6ce@gmail.com>
2023-03-27 20:32:25 +0900

    お天気アプリ

--- js-test/js/script.js
+++ js-test/js/script.js
@@ -1,83 +1,81 @@
 const textArray = [
-    "明日は晴れるかな。",
-    "おはようございます。",
-    "ありがとう、助かった。",
-    "日本語が上手ですね。",
-    "今日も一日頑張ろう。",
-    "食べ物が美味しいです。",
-    "新しい友達が欲しい。",
-    "旅行に行きたいですね。",
-    "家族と過ごす時間が好き。",
-    "音楽を聴くのが好きです。",
-    "スポーツは健康に良い。",
-    "本を読むのが好きです。",
-    "今日は何をしようか。",
-    "何か楽しいことがあるといい。",
-    "夢を追いかけて頑張ろう。",
-    "美しい景色を見たいです。",
-    "お腹がすいた、何か食べたい。",
-    "笑うことが大切だと思います。",
-    "冬は寒くて嫌いです。",
-    "春が来た、桜が綺麗ですね。"
-  ];
-  
-  let currentText = "";
-  let score = 0;
-  
-  const textElement = document.getElementById("text");
-  const inputElement = document.getElementById("input");
-  const scoreElement = document.getElementById("score");
-  
-  function getRandomText() {
-    const index = Math.floor(Math.random() * textArray.length);
-    return textArray[index];
-  }
-  
-  function updateText() {
-    currentText = getRandomText();
-    textElement.textContent = currentText;
-    inputElement.value = "";
-  }
-  
-  function calculateScore() {
-    const correctChars = inputElement.value.split("").filter((char, i) => {
-      return char === currentText[i];
-    }).length;
-    const accuracy = correctChars / currentText.length;
-    const timeBonus = Math.max(0, (30 - timeLeft) / 30);
-    const score = Math.floor(accuracy * 100 * timeBonus);
-    return score;
-  }
-  
-  function handleInput() {
-    if (inputElement.value === currentText) {
-      score += calculateScore();
-      scoreElement.textContent = `Score: ${score}`;
-      updateText();
-    }
-  }
-  
+  "こんにちは",
+  "おはよう",
+  "さようなら",
+  "ありがとう",
+  "おやすみ"
+];
 
-  
-  
-  function startGame() {
-    updateText();
-    inputElement.addEventListener("input", handleInput);
-  }
-  
-  function endGame() {
-    inputElement.removeEventListener("input", handleInput);
-    const scoreText = `Final score: ${score}`;
-    alert(scoreText);
-    updateText();
-    score = 0;
+let currentText = "";
+let score = 0;
+let timeLeft = 30;
+let intervalId = null;
+
+const textElement = document.getElementById("text");
+const inputElement = document.getElementById("input");
+const scoreElement = document.getElementById("score");
+const startButton = document.getElementById("start-button");
+const resetButton = document.getElementById("reset-button");
+const timeLeftElement = document.getElementById("time-left");
+
+function getRandomText() {
+  const index = Math.floor(Math.random() * textArray.length);
+  return textArray[index];
+}
+
+function updateText() {
+  currentText = getRandomText();
+  textElement.textContent = currentText;
+  inputElement.value = "";
+}
+
+function calculateScore() {
+  const correctChars = inputElement.value.split("").filter((char, i) => {
+    return char === currentText[i];
+  }).length;
+  const accuracy = correctChars / currentText.length;
+  const timeBonus = Math.max(0, timeLeft / 30);
+  const score = Math.floor(accuracy * 100 * timeBonus);
+  return score;
+}
+
+function handleInput() {
+  if (inputElement.value === currentText) {
+    score += calculateScore();
     scoreElement.textContent = `Score: ${score}`;
+    updateText();
   }
-  
-  function init() {
-    startGame();
-    setInterval(endGame, 30000);
-  }
-  
-  init();
-  
+}
+
+function startGame() {
+  updateText();
+  inputElement.addEventListener("input", handleInput);
+  startButton.disabled = true;
+  intervalId = setInterval(() => {
+    timeLeft -= 1;
+    timeLeftElement.textContent = timeLeft.toString();
+    if (timeLeft === 0) {
+      endGame();
+    }
+  }, 1000);
+}
+
+function resetGame() {
+  clearInterval(intervalId);
+  timeLeft = 30;
+  timeLeftElement.textContent = timeLeft.toString();
+  score = 0;
+  scoreElement.textContent = `Score: ${score}`;
+  startButton.disabled = false;
+}
+
+function endGame() {
+  clearInterval(intervalId);
+  inputElement.removeEventListener("input", handleInput);
+  const scoreText = `Final score: ${score}`;
+  alert(scoreText);
+  resetGame();
+}
+
+startButton.addEventListener("click", startGame);
+resetButton.addEventListener("click", resetGame);
