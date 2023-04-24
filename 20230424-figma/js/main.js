8ba1abbbad2d6b690350ac33e341e0177ec566a3
h.ogino.258 <ogna6ce@gmail.com>
2023-04-24 20:55:00 +0900

    Figmaテスト追加

---
+++ 20230424-figma/js/main.js
@@ -0,0 +1,33 @@
+function testModal() {
+
+  const modal = document.querySelector('.modal');
+  const overlay = document.querySelector('.modal-overlay');
+  const closeButton = document.querySelector('.modal-close');
+  const openButton = document.querySelector('.modal-open');
+  
+  function openModal() {
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+  }
+  
+  function closeModal() {
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+  }
+  
+  
+  openButton.addEventListener('click', openModal);
+  closeButton.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+
+}
+
+window.addEventListener('DOMContentLoaded', testModal());
+
+
+// window.addEventListener('keydown', (event) => {
+//   if (event.key === 'Escape') {
+//     closeModal();
+//   }
+// });
+
